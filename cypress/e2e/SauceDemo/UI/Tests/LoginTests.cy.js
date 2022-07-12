294fcdd836738e49a25948bab802a423ace8be1c
iEnergyy <itsenergyy05@gmail.com>
2022-07-12 18:01:51 -0400

    fixed a typo and modified the places where it was used

--- cypress/e2e/SauceDemo/UI/Tests/LoginTests.cy.js
+++ cypress/e2e/SauceDemo/UI/Tests/LoginTests.cy.js
@@ -8,12 +8,12 @@ import LoginPage from '../PageObjects/LoginPage';
 describe('Login test', () => {
 
     it('Logs in successfully to the web app', () => {
-        LoginPage.Login(settings.saude_demo.credentials.standard_user, settings.saude_demo.credentials.password);
+        LoginPage.Login(settings.sauce_demo.credentials.standard_user, settings.sauce_demo.credentials.password);
         InventoryPage.TitleField().contains('Products');
     });
 
     it('Locked user unable to login', () => {
-        LoginPage.Login(settings.saude_demo.credentials.locked_out_user, settings.saude_demo.credentials.password);
+        LoginPage.Login(settings.sauce_demo.credentials.locked_out_user, settings.sauce_demo.credentials.password);
         LoginPage.ErrorMsgContainer().contains('Sorry, this user has been locked out.');
         cy.url().should('eq', 'https://www.saucedemo.com/');
     });
