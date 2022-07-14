ed2e0d77d38d19bbab6af3eb1c01f59e0a11974b
iEnergyy <itsenergyy05@gmail.com>
2022-07-14 10:00:20 -0400

    added validation for performance issue user

--- cypress/e2e/SauceDemo/UI/Tests/LoginTests.cy.js
+++ cypress/e2e/SauceDemo/UI/Tests/LoginTests.cy.js
@@ -1,46 +1,59 @@
 /// <reference types="cypress" />
-import Stopwatch from 'timer-stopwatch/lib/Stopwatch';
-import settings from '../../../../fixtures/settings.json'
+import Stopwatch from "timer-stopwatch/lib/Stopwatch";
+import settings from "../../../../fixtures/settings.json";
 
-import InventoryPage from '../PageObjects/InventoryPage';
-import LoginPage from '../PageObjects/LoginPage';
+import InventoryPage from "../PageObjects/InventoryPage";
+import LoginPage from "../PageObjects/LoginPage";
 
+describe("Login test", () => {
+  it("Logs in successfully to the web app", () => {
+    LoginPage.Login(
+      settings.sauce_demo.credentials.standard_user,
+      settings.sauce_demo.credentials.password
+    );
+    InventoryPage.TitleField().contains("Products");
+  });
 
-describe('Login test', () => {
+  it("Locked user unable to login", () => {
+    LoginPage.Login(
+      settings.sauce_demo.credentials.locked_out_user,
+      settings.sauce_demo.credentials.password
+    );
+    LoginPage.ErrorMsgContainer().contains(
+      "Sorry, this user has been locked out."
+    );
+    cy.url().should("eq", "https://www.saucedemo.com/");
+  });
 
-    it('Logs in successfully to the web app', () => {
-        LoginPage.Login(settings.sauce_demo.credentials.standard_user, settings.sauce_demo.credentials.password);
-        InventoryPage.TitleField().contains('Products');
+  it("Logs in with problem user", () => {
+    LoginPage.Login(
+      settings.sauce_demo.credentials.problem_user,
+      settings.sauce_demo.credentials.password
+    );
+    InventoryPage.TitleField().contains("Products");
+    InventoryPage.InventoryItemImgs().each(($item) => {
+      cy.wrap($item).should(
+        "have.attr",
+        "src",
+        "/static/media/sl-404.168b1cce.jpg"
+      );
     });
+  });
 
-    it('Locked user unable to login', () => {
-        LoginPage.Login(settings.sauce_demo.credentials.locked_out_user, settings.sauce_demo.credentials.password);
-        LoginPage.ErrorMsgContainer().contains('Sorry, this user has been locked out.');
-        cy.url().should('eq', 'https://www.saucedemo.com/');
+  it("Logs in with performance glitch user", () => {
+    LoginPage.Login(
+      settings.sauce_demo.credentials.performance_glitch_user,
+      settings.sauce_demo.credentials.password
+    );
+    const startTime = performance.now();
+    cy.log("Start time: " + startTime);
+    InventoryPage.TitleField().then(($titleField) => {
+      const endTime = performance.now();
+      cy.wrap($titleField).contains("Products");
+      cy.log("End time: " + endTime);
+      const elapsedTimeMs = endTime - startTime;
+      cy.log("Elapsed time (ms): " + elapsedTimeMs);
+      cy.wrap(elapsedTimeMs).should("be.at.least", 4000);
     });
-
-    it('Logs in with problem user', () => {
-        LoginPage.Login(settings.sauce_demo.credentials.problem_user, settings.sauce_demo.credentials.password);
-        InventoryPage.TitleField().contains('Products');
-        InventoryPage.InventoryItemImgs().each(($item)=>{
-            cy.wrap($item).should('have.attr', 'src', '/static/media/sl-404.168b1cce.jpg');
-        })
-    });
-
-    it('Logs in with performance glitch user', () => {
-        // LoginPage.Login(settings.saude_demo.credentials.performance_glitch_user, settings.saude_demo.credentials.password);
-        var startTime = Date.now();
-        LoginPage.Navigate();
-        LoginPage.UsernameInput().type(settings.sauce_demo.credentials.performance_glitch_user);
-        LoginPage.PasswordInput().type(settings.sauce_demo.credentials.password);
-        //start timer
-        LoginPage.LoginButon().click();
-        InventoryPage.TitleField().contains('Products');
-        //stop timer
-        var endTime = Date.now();
-        cy.log(startTime);
-        cy.log(endTime);
-        cy.log(endTime - startTime);
-        //expect that it takes 4 or more seconds
-    });
+  });
 });
